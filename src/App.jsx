198536d41d556b5d51feaa98b95f9cc54a9e07b7
bryan7231 <lolcivilday@gmail.com>
2024-11-02 23:17:01 -0400

    Made the card a fixed size and fixed a lot of spacing. There are issues with the spacing when the title or description is especially long.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./App.css";
-import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Spinner from "react-bootstrap/Spinner";
 import Form from "react-bootstrap/Form";
+import "./App.css";
+import "./index.css";
 
 function App() {
   const [movieData, setMovieData] = useState({});
@@ -114,14 +114,14 @@ function App() {
   return (
     <div className="app container fixed-height-card">
       {loading ? ( // Conditional rendering for loading state
-        <div className="loading-screen fade-in-up">
+        <div className="center loading-screen fade-in-up">
           <Spinner animation="border" role="status">
             <span className="visually-hidden fade-in-up">Loading...</span>
           </Spinner>
           <p>Loading...</p>
         </div>
       ) : showForm ? ( // Render the form if showForm is true
-        <Form onSubmit={handleSubmit} className="mb-2">
+        <Form onSubmit={handleSubmit} className="center mb-2">
           <Form.Group controlId="formMovieTitle">
             <Form.Label>Enter Movie Title</Form.Label>
             <Form.Control
@@ -137,82 +137,74 @@ function App() {
         </Form>
       ) : (
         movieData && ( // Render the card if movieData is available
-          <div className="row justify-content-center container">
-            <div className="col-md-5">
-              {" "}
-              {/* Adjust the column size as needed */}
-              <Card className={`mb-3 ${fadeOut ? "fade-out" : "fade-in-up"}`}>
-                <h1 id="title">{movieData.title}</h1>
-                <h2 id="year">
-                  {movieData.release_date
-                    ? movieData.release_date.slice(0, 4)
-                    : ""}
-                </h2>
-                <Card.Img
-                  className="w-40"
-                  src={`https://image.tmdb.org/t/p/w342${movieData.poster_path}`}
-                  alt="Movie Poster"
-                />
-
-                <h3 id="director">
-                  {credits.crew
-                    ? credits.crew.find(
-                        (member) => member.known_for_department === "Directing"
-                      )?.name
-                    : ""}
-                </h3>
-                <h3 id="cast">
-                  {credits.cast
-                    ? credits.cast
-                        .slice(0, 3)
-                        .map((actor) => actor.name)
-                        .join(", ")
-                    : ""}
-                </h3>
-                <p id="desc" className="truncated-text">
-                  {movieData.overview}
-                </p>
-                <Button
-                  variant="success"
-                  onClick={fetchMovieData}
-                  className="btn-sm w-40 mx-auto d-block"
-                >
-                  Next
-                </Button>
-                {error && <p>{error}</p>}
-                <div className="buttons">
-                  <svg
-                    id="expandable-svg"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-x-circle-fill"
-                    viewBox="0 0 32 32"
-                    onClick={() => {
-                      fetchMovieData(false);
-                    }}
-                  >
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
-                  </svg>
-                  <svg
-                    id="expandable-svg"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-check-circle-fill"
-                    viewBox="0 0 32 32"
-                    onClick={() => {
-                      fetchMovieData(true);
-                    }}
-                  >
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-                  </svg>
-                </div>
-              </Card>
+          
+          
+          <Card className={`card  ${fadeOut ? "fade-out" : "fade-in-up"}`}>
+            <div>
+              <h1 className="" id="title">{movieData.title}</h1>
+              <h2 id="year">
+                {movieData.release_date
+                  ? movieData.release_date.slice(0, 4)
+                  : ""}
+              </h2>
             </div>
-          </div>
+            <Card.Img
+              className="cardImg "
+              src={`https://image.tmdb.org/t/p/w342${movieData.poster_path}`}
+              alt="Movie Poster"
+            />
+
+            <h3 id="director" className="">
+              {credits.crew
+                ? credits.crew.find(
+                    (member) => member.known_for_department === "Directing"
+                  )?.name
+                : ""}
+            </h3>
+            <h4 id="cast" className="text pt-3 pb-3 border-top border-bottom">
+              {credits.cast
+                ? credits.cast
+                    .slice(0, 3)
+                    .map((actor) => actor.name)
+                    .join(", ")
+                : ""}
+            </h4>
+            <p id="desc" className="text">
+              {movieData.overview}
+            </p>
+            {error && <p>{error}</p>}
+            <div className="buttons mb-2">
+              <svg
+                id="expandable-svg"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-x-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={() => {
+                  fetchMovieData(false);
+                }}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+              </svg>
+              <div class="filler"></div>
+              <svg
+                id="expandable-svg"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-check-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={() => {
+                  fetchMovieData(true);
+                }}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+              </svg>
+            </div>
+          </Card>
         )
       )}
     </div>
