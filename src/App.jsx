17229017f0709467495c4d22519f6cc27e2629da
chzzou92 <chzzou92@gmail.com>
2024-11-08 23:35:50 -0500

    progressbar added

--- src/App.jsx
+++ src/App.jsx
@@ -161,8 +161,11 @@ function App() {
       ) : showSelect ? (
         <>
           <Select movies={selectedMovies} />
-          <ShoppingCart showSelect={showSelect} setShowSelect={setShowSelect} movies={selectedMovies}/>
-          <MovieProgressBar/>
+          <ShoppingCart
+            showSelect={showSelect}
+            setShowSelect={setShowSelect}
+            movies={selectedMovies}
+          />
         </>
       ) : (
         movieData && ( // Render the card if movieData is available
@@ -202,7 +205,12 @@ function App() {
                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
               </svg>
             </div>
-            <ShoppingCart showSelect={showSelect} setShowSelect={setShowSelect} movies={selectedMovies} fadeOut={fadeOut}/>
+            <ShoppingCart
+              showSelect={showSelect}
+              setShowSelect={setShowSelect}
+              movies={selectedMovies}
+              fadeOut={fadeOut}
+            />
           </>
         )
       )}
--- src/Select.jsx
+++ src/Select.jsx
@@ -1,15 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import MovieCard from "./MovieCard.jsx";
+import React, { useRef } from "react";
+import MovieProgressBar from "./MovieProgressbar.jsx";
 
 function Select({ movies }) {
-
-
-
   return (
-    <div className={`selectApp ${movies.length === 1 ? "center" : ""}`} style={movies.length===2 ? {width: "70%"} : {}}>
+    <div
+      className={`selectApp ${movies.length === 1 ? "center" : ""}`}
+      style={movies.length === 2 ? { width: "70%" } : {}}
+    >
       {movies.length === 0 ? <h1>Nothing here buddy</h1> : null}
-      {movies.slice(0, 3).map((movie) => (
+      {movies.map((movie) => (
         <MovieCard
           key={movie.movieID}
           movieID={movie.movieID}
@@ -18,12 +20,13 @@ function Select({ movies }) {
           credits={movie.movieCredits}
         />
       ))}
+      <MovieProgressBar />
     </div>
   );
 }
 
 Select.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object).isRequired
-}
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Select;
