10a473bd263bb57b9f4cbcccf394ebf6925cf387
chzzou92 <chzzou92@gmail.com>
2024-12-16 15:18:37 -0500

    fixing

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/chzzou92.github.io/", // Set this to the repository name for GitHub Pages
+  base: "/scenethis/", 
 });
